18b2d4809af5339cf6afe8b80742204520d4fe65
Reet <71899877+ReetRatri@users.noreply.github.com>
2024-12-01 02:55:13 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,28 @@
+from typing import Dict
+import time
+class DataStream:
+    def __init__(self):
+        self.last_seen: Dict[str, int] = {}
+
+    def should_output_data_str(self, timestamp: int, data_str: str) -> bool:
+        
+        if data_str not in self.last_seen or timestamp - self.last_seen[data_str] >= 5:
+           
+            self.last_seen[data_str] = timestamp
+            
+            return True
+        return False
+
+data_stream = DataStream()
+
+# Test with the provided sequence
+test_input = [
+    (0, "hello"),
+    (1, "world"),
+    (6, "hello"),
+    (7, "hello"),
+    (8, "world")
+]
+
+output = [data_stream.should_output_data_str(timestamp, data_string) for timestamp, data_string in test_input]
+print(output)  
---
+++ problem2.py
@@ -0,0 +1,36 @@
+class FuelStation:
+    def __init__(self, diesel: int, petrol: int, electric: int):
+        self.spots = {
+            "diesel": diesel,
+            "petrol": petrol,
+            "electric": electric
+        }
+
+    def fuel_vehicle(self, fuel_type: str) -> bool:
+       
+        if self.spots.get(fuel_type, 0) > 0:
+          
+            self.spots[fuel_type] -= 1
+            return True
+        return False
+
+    def open_fuel_slot(self, fuel_type: str) -> bool:
+        
+        if self.spots.get(fuel_type, 0) < 0:
+            return False
+        self.spots[fuel_type] += 1
+        return True
+
+fuel_station = FuelStation(diesel=2, petrol=2, electric=1)
+
+print(fuel_station.fuel_vehicle("diesel"))    
+print(fuel_station.fuel_vehicle("petrol"))    
+print(fuel_station.fuel_vehicle("diesel"))    
+print(fuel_station.fuel_vehicle("electric"))  
+print(fuel_station.fuel_vehicle("diesel"))    
+
+
+print(fuel_station.open_fuel_slot("diesel"))  
+print(fuel_station.fuel_vehicle("diesel"))    
+print(fuel_station.open_fuel_slot("electric"))  
+print(fuel_station.open_fuel_slot("electric"))  
